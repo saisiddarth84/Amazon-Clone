b2a040079029a640884d82b107b82b39a7cb2ce3
Sai Siddarth <siddarthsiddu086@gmail.com>
2024-02-26 14:13:10 +0530

    Render the HTML for checkout header and display it on the page

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -2,12 +2,29 @@ import {calculateCartQuantity} from '../../data/cart.js'
 
 export function renderCheckoutHeader(){
   const cartQuantity = calculateCartQuantity();
-  const html = `
-  Checkout (<a class="return-to-home-link js-return-to-home-link"
-    href="amazon.html">${cartQuantity} items</a>)
-  `
-  document.querySelector('.checkout-header-middle-section')
-    .innerHTML = html;
+ 
+  const checkoutHeaderHTML = `
+      <div class="header-content">
+        <div class="checkout-header-left-section">
+          <a href="amazon.html">
+            <img class="amazon-logo" src="images/amazon-logo.png">
+            <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
+          </a>
+        </div>
+        <div class="checkout-header-middle-section">
+          Checkout (<a class="return-to-home-link"
+            href="amazon.html">${cartQuantity} items</a>)
+        </div>
+        <div class="checkout-header-right-section">
+          <img src="images/icons/checkout-lock-icon.png">
+        </div>
+      </div>
+    `;
+
+    const element = document.querySelector('.js-checkout-header');
+    if (element) {
+       element.innerHTML = checkoutHeaderHTML;
+    }
 }
 
 renderCheckoutHeader();
