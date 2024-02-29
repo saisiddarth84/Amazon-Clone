26038306a3e62964bfb5cca20abe31525e97d5a3
Sai Siddarth <siddarthsiddu086@gmail.com>
2024-02-29 23:25:58 +0530

    Make search input interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,64 +2,68 @@ import {cart, addToCart, calculateCartQuantity} from '../data/cart.js'
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js'
 
-let productsHTML = '';
+function renderProductContainer(products) {
+  let productsHTML = '';
 
-products.forEach((product) => {
-  productsHTML += `
-      <div class="product-container">
-        <div class="product-image-container">
-          <img class="product-image"
-            src="${product.image}">
-        </div>
-
-        <div class="product-name limit-text-to-2-lines">
-          ${product.name}
-        </div>
-
-        <div class="product-rating-container">
-          <img class="product-rating-stars"
-            src="images/ratings/rating-${product.rating.stars * 10}.png">
-          <div class="product-rating-count link-primary">
-            ${product.rating.count}
+  products.forEach((product) => {
+    productsHTML += `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
           </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${formatCurrency(product.priceCents)}
+          </div>
+          
+          <div class="product-quantity-container">
+            <select class="js-quantity-selector-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart js-added-cart-${product.id}">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
+            Add to Cart
+          </button>
         </div>
+    `;
+  })
 
-        <div class="product-price">
-          $${formatCurrency(product.priceCents)}
-        </div>
-         
-        <div class="product-quantity-container">
-          <select class="js-quantity-selector-${product.id}">
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-        <div class="product-spacer"></div>
+}
 
-        <div class="added-to-cart js-added-cart-${product.id}">
-          <img src="images/icons/checkmark.png">
-          Added
-        </div>
-
-        <button class="add-to-cart-button button-primary js-add-to-cart"
-        data-product-id="${product.id}">
-          Add to Cart
-        </button>
-      </div>
-  `;
-})
-
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
+renderProductContainer(products);
 function updateCartQuantity(){
   const cartQuantity = calculateCartQuantity();
 
@@ -95,3 +99,10 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     })
   })
 })
+
+document.querySelector('.js-search-bar')
+  .addEventListener('input', (e) => {
+    const word = e.target.value;
+    const findProducts = products.filter((product) => product.name.toLowerCase().includes(word.toLowerCase()))
+    renderProductContainer(findProducts)
+  })
